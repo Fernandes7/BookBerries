f6a7b4332878ea01edebb81b27b7d75a36475e97
FernoLinux <fernooxplozd@gmail.com>
2024-04-22 23:25:03 +0530

    Completed Backend by Ferno

--- src/models/reviewSchema.js
+++ src/models/reviewSchema.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const reviewSchema = new mongoose.Schema({
   bookid: { type: mongoose.Schema.Types.ObjectId, ref: "Bookdata" },
+  userid:{type:mongoose.Schema.Types.ObjectId, ref: "User"},
   bookreview:{type:String},
   bookscore:{type:Number}
-});
+},{timestamps:true});
 
 const RewiewSchema=mongoose.model("Reviews",reviewSchema)
 
